a7d0b42f2eaa894fa2b8757f71d800c9837ecff2
Nauh24 <nvnauh@gmail.com>
2023-02-14 17:40:05 +0700

    TÌM KIẾM

---
+++ tim_kiem.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+int t;cin>>t;
+while(t--){
+int n,x;
+cin>>n>>x;
+int a[n];
+map<int,int> mp;
+for(int i=0;i<n;i++){
+    cin>>a[i];
+    mp[a[i]]++;
+}
+if(mp[x]==0) cout<<-1;
+else cout<<1;
+    cout<<endl;
+}
+    system("pause");
+}
