8674afd19f4a1935eb3cbe7cfaa8eb322d3be2f6
Nauh24 <nvnauh@gmail.com>
2023-02-15 20:48:08 +0700

    DSA06023	SẮP XẾP ĐỔI CHỖ TRỰC TIẾP

---
+++ sap_xep_doi_cho_truc_tiep.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+int n;cin>>n;
+int a[n];
+for(int i=0;i<n;i++) cin>>a[i];
+for(int i=0;i<n-1;i++){
+    for(int j=i+1;j<n;j++){
+        if(a[i]>a[j]) swap(a[i],a[j]);
+    }
+    cout<<"Buoc "<<i+1<<": ";
+    for(int i=0;i<n;i++) cout<<a[i]<<" ";
+    cout<<endl;
+}
+    system("pause");
+}
