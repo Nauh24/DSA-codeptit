dc9ac67047ab848cb271c09ea6b90b61a8911e28
Nauh <117579240+Nauh24@users.noreply.github.com>
2023-03-19 15:15:09 +0700

    Update sap_xep_cong_viec_2.cpp

--- sap_xep_cong_viec_2.cpp
+++ sap_xep_cong_viec_2.cpp
@@ -1,53 +1,36 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-int n;
-bool cmp(pair<int, int> a, pair<int, int> b)
-{
-    if (a.second == b.second)
-        return a.first < b.first;
-    return a.second > b.second;
+struct Job{
+	int id,deadline,profit;
+};
+bool cmp(Job a,Job b){
+	return a.deadline>b.deadline;
 }
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        cin >> n;
-        int x;
-        vector<pair<int, int>> v(n);
-        for (int i = 0; i < n; i++)
-        {
-            cin >> x;
-            cin >> v[i].first;
-            cin >> v[i].second;
-        }
-        sort(v.begin(), v.end(), cmp);
-        // for (int i = 0; i < n; i++)
-        // {
-        //     cout << v[i].first << " " << v[i].second << endl;
-        // }
-        int sum = 0, cnt = 0;
-        bool used[1001] = {false};
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = v[i].first; j > 0; j--)
-            {
-                if (!used[j])
-                {
-                    sum += v[i].second;
-                    cnt++;
-                    used[j] = true;
-                    break;
-                }
-            }
-        }
-        cout << cnt << " " << sum;
-        cout << endl;
-    }
-    system("pause");
+int main(){
+int t;cin>>t;
+while(t--){
+	int n;cin>>n;
+	Job a[n];
+	for(int i=0;i<n;i++) {
+		cin>>a[i].id>>a[i].deadline>>a[i].profit;
+	}
+	sort(a,a+n,cmp);
+	priority_queue <int> pq;
+	int cur=a[0].deadline;
+	int index=0,cnt=0,sum=0;
+	for(int i=cur;i>=1;i--){
+		while(index<n&&a[index].deadline>=i){
+			pq.push(a[index].profit);
+			index++;
+		}
+		if(!pq.empty()){
+			sum+=pq.top();
+			cnt++;
+			pq.pop();
+		}
+	}
+	cout<<cnt<<" "<<sum;
+	cout<<endl;
+}
 }
