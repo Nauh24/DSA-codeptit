bbec7f199041e4fc86c979673cd27869071a465c
Nauh24 <nvnauh@gmail.com>
2023-04-08 09:06:55 +0700

    DSA05001	XÂU CON CHUNG DÀI NHẤT

---
+++ xau_con_chung_dai_nhat.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1;
+        cin >> s1;
+        string s2;
+        cin >> s2;
+        int n = s1.size();
+        int m = s2.size();
+        int d[n + 1][m + 1];
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j <= m; j++)
+            {
+                if (i == 0 || j == 0)
+                    d[i][j] = 0;
+                else
+                {
+                    if (s1[i - 1] == s2[j - 1])
+                        d[i][j] = d[i - 1][j - 1] + 1;
+                    else
+                        d[i][j] = max(d[i - 1][j], d[i][j - 1]);
+                }
+            }
+        }
+        cout << d[n][m];
+        cout << endl;
+    }
+    system("pause");
+}
