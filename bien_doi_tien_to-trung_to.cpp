d99c91ae88e3f833a5428a42566ad72e41e4084f
Nauh24 <nvnauh@gmail.com>
2023-04-21 21:58:56 +0700

    DSA07009 - BIẾN ĐỔI TIỀN TỐ - TRUNG TỐ

---
+++ bien_doi_tien_to-trung_to.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        stack<string> st;
+        for (int i = s.size() - 1; i >= 0; i--)
+        {
+            if (s[i] >= 'A' && s[i] <= 'Z')
+            {
+                st.push(string(1, s[i]));
+            }
+            else
+            {
+                string a = st.top();
+                st.pop();
+                string b = st.top();
+                st.pop();
+                st.push('(' + a + s[i] + b + ')');
+            }
+        }
+        cout << st.top();
+        cout << endl;
+    }
+    system("pause");
+}
