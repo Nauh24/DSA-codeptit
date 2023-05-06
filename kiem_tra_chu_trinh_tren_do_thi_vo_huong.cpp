e38bd4eef7caf6d5f27ca2761be8646486b3cab0
Nauh24 <nvnauh@gmail.com>
2023-05-06 20:46:26 +0700

    DSA09014 - KIỂM TRA CHU TRÌNH TRÊN ĐỒ THỊ VÔ HƯỚNG

---
+++ kiem_tra_chu_trinh_tren_do_thi_vo_huong.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int n,m;
+vector<int> v[1001];
+bool visited[1001];
+int parent[1001];
+bool Dfs(int u){
+	visited[u]=true;
+	for(auto it:v[u]){
+		if(!visited[it]){
+			parent[it]=u;
+			if(Dfs(it)) return true;
+		}
+		else if(it!=parent[u]) return true;
+	}
+	return false;
+}
+int main(){
+	int t;cin>>t;
+	while(t--){
+		cin>>n>>m;
+		for(int i=0;i<1001;i++) v[i].clear();
+		memset(visited,false,sizeof(visited));
+		memset(parent,0,sizeof(parent));
+		while(m--){
+			int x,y;
+			cin>>x>>y;
+			v[x].push_back(y);
+			v[y].push_back(x);
+		}
+		int ok=0;
+		for(int i=1;i<=n;i++){
+			if(!visited[i]&&Dfs(i)){
+				ok=1;
+				cout<<"YES";
+				break;
+			}
+		}
+		if(ok==0) cout<<"NO";
+		cout<<endl;
+	}
+}
+
+/*
+
+*/
